26ac1f028cd1f3185758d0661c38816de635ec21
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-03-01 14:55:06 +0100

    crud

---
+++ app/Http/Controllers/Admin/EventController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Tag;
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $events = Event :: all();
+
+        return view('events.index', compact('events'));
+
+        }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $tags = Tag :: all();
+        
+        return view('events.create', compact('tags'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request -> all();
+        
+
+        $event = new Event;
+
+        $event -> title = $data['title'];
+        $event -> description = $data['description'];
+        $event -> save();
+        $event -> tags() -> attach($data['tags']);
+
+        return redirect() -> route('events.show', $event -> id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $event = Event :: find($id);
+
+        return view('events.show', compact('event'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $event = Event :: find($id);
+        $tags = Tag :: all();
+
+        return view('events.edit', compact('event', 'tags'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request -> all();
+
+        $event = Event :: find($id);
+
+        $event -> title = $data['title'];
+        $event -> description = $data['description'];
+
+        $event -> save();
+
+        $event -> tags() -> sync($data['tags']);
+        return redirect() -> route('events.show', $event -> id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $event = Event :: find($id);
+        $event -> tags() -> detach();
+        $event -> delete();
+        return redirect() -> route('events.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\EventController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,10 +18,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', [EventController::class, 'index']) ->name('events.index');
+Route::get('events/create', [EventController::class, 'create']) ->name('events.create');
+Route::post('/events',[EventController::class,'store'])->name('events.store');
+Route :: delete('/events/{id}', [EventController :: class, 'destroy'])-> name('events.destroy');
+Route :: get('/events/{id}/edit', [EventController :: class, 'edit'])-> name('events.edit');
+Route :: patch('/events/{id}', [EventController :: class, 'update'])-> name('events.update');
+Route :: get('/events/{id}', [EventController :: class, 'show'])-> name('events.show');
+
+
+
+// // //  //  //  //  
+
+
+
+
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+// 
+// 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
